1acef9cb3bd2876753e207db8c2499cf32c73428
brenki1 <pagioliosvaldo@gmail.com>
2025-04-08 23:53:52 -0300

    Algoritmo recursivo pronto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let coins: [i32; 6] = [1, 5, 10,25,50,100];
+
+    let troco = 80;
+
+    let inicio = Instant::now();
+    let resultado = alg_troco_rec(coins, 80);
+    let fim: Instant = Instant::now();
+
+    println!("O numero de moedas para o troco de {} foi: {}, e o tempo de execução foi de {:?}", (troco/100), resultado, (fim-inicio));
+}
+
+fn alg_troco_rec(moedas: [i32;6], t: i32) -> i32 {
+    let mut min_moedas: i32 = t;
+
+    if moedas.contains(&t) {
+        return 1;
+    }
+
+    for &coin in moedas.iter().filter(|&&c| c <= t) {
+        let num_moedas = 1 + alg_troco_rec(moedas, t - coin);
+        if num_moedas < min_moedas {
+            min_moedas = num_moedas;
+        }
+    }
+
+    return min_moedas;
 }
